ee11c75dd5a5ee1e20db022228c75bddc962ea4c
SebaChuaqui <schuaquicano@gmail.com>
2022-05-29 18:00:26 -0300

    Videos III

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,3 +28,31 @@ let modulo = (() => {
     }
 });
 
+// Clase Padre protegiendo el atributo con closures
+
+class Videos {
+    constructor(url) {
+        this._url = url;
+        this.getUrl = () => _url;
+        this.setUrl = (new_url) => _url = new_url;
+    }
+
+    get url() {
+        return this.getUrl();
+    }
+
+    set url(new_url) {
+        this.setUrl(new_url);
+    }
+
+    setComienzo() {
+        return 'Método para realizar un cambio en la URL del Video'
+    }
+
+}
+
+    // Clase Hija
+
+class Reproduccion extends Videos{
+    
+}
